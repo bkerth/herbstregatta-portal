8a690f60ac1dd7aee68a12eddbd72b1165d458e0
Kerth <cas289912@cas.dhbw.de>
2022-10-30 16:42:37 +0100

    Rename to Herbstregatta.Portal

--- src/Herbstregatta.Portal/Properties/Resource.Designer.cs
+++ src/Herbstregatta.Portal/Properties/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Herbstregatta.Manager.Properties {
+namespace Herbstregatta.Portal.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Herbstregatta.Manager.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Herbstregatta.Manager.Properties.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Herbstregatta.Portal.Properties.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
